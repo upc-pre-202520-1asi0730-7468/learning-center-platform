bcbd7ca2165662c12a7138331b6514baa0ef243a
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-28 15:26:49 -0500

    feat(shared): add shared bounded context.

---
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,22 @@
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        // Add the created and updated interceptor
+        builder.AddCreatedUpdatedInterceptor();
+        base.OnConfiguring(builder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Use snake case for database objects and pluralization for table names
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
---
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Repositories/BaseRepository.cs
@@ -0,0 +1,41 @@
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+{
+    protected readonly AppDbContext Context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        Context = context;
+    }
+
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await Context.Set<TEntity>().AddAsync(entity);
+    }
+
+    public async Task<TEntity?> FindByIdAsync(int id)
+    {
+        return await Context.Set<TEntity>().FindAsync(id);
+    }
+
+    public void Update(TEntity entity)
+    {
+        Context.Set<TEntity>().Update(entity);
+    }
+
+    public void Remove(TEntity entity)
+    {
+        Context.Set<TEntity>().Remove(entity);
+    }
+
+    public async Task<IEnumerable<TEntity>> ListAsync()
+    {
+        return await Context.Set<TEntity>().ToListAsync();
+    }
+}
