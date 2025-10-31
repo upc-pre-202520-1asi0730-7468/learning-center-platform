1c0e2810d625fdd7af8a9972b8f8bb4ea8cc73a9
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-30 21:49:00 -0500

    feat(publishing): add category-repository and tutorial-repository interfaces for category and tutorial management.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Repositories/ITutorialRepository.cs
@@ -0,0 +1,11 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Repositories;
+
+public interface ITutorialRepository : IBaseRepository<Tutorial>
+{
+    Task<IEnumerable<Tutorial>> FindByCategoryIdAsync(int categoryId);
+    
+    Task<bool> ExistsByTitleAsync(string title);
+}
