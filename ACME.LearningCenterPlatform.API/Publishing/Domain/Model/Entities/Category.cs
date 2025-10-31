2ed5d6afc295e82f1e255646ef32e461e950c1ac
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-30 20:39:13 -0500

    feat(publishing): implement category entity and create-category-command for category management.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
@@ -0,0 +1,13 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Commands;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+public class Category(string name)
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = name;
+
+    public Category() : this(string.Empty) {}
+
+    public Category(CreateCategoryCommand command) : this(command.Name) {} 
+}
