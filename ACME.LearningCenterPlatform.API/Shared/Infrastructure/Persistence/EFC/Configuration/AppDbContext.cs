e7526bebd157a191ef00af59d2ed73cae475a779
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-11 15:57:01 -0500

    feat(database): add iam context configuration to the application database context.

--- ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,3 +1,4 @@
+using ACME.LearningCenterPlatform.API.IAM.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using ACME.LearningCenterPlatform.API.Profiles.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using ACME.LearningCenterPlatform.API.Publishing.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
@@ -6,13 +7,13 @@ using Microsoft.EntityFrameworkCore;
 namespace ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 
 /// <summary>
-/// Represents the application's database context using Entity Framework Core.
+///     Represents the application's database context using Entity Framework Core.
 /// </summary>
 /// <param name="options">The options for configuring the context.</param>
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     /// <summary>
-    /// Configures the database context options.
+    ///     Configures the database context options.
     /// </summary>
     /// <param name="builder">The options' builder.</param>
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
@@ -23,7 +24,7 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     }
 
     /// <summary>
-    /// Configures the model for the database context.
+    ///     Configures the model for the database context.
     /// </summary>
     /// <param name="builder">The model builder.</param>
     protected override void OnModelCreating(ModelBuilder builder)
@@ -35,6 +36,9 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
 
         // Profiles Context
         builder.ApplyProfilesConfiguration();
+
+        // IAM Context
+        builder.ApplyIamConfiguration();
         
         // Use snake case for database objects and pluralization for table names
         builder.UseSnakeCaseNamingConvention();
